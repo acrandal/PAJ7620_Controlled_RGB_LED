feda9ee7ad3ff46aedee79251c932e7379ff1cbb
Aaron S. Crandall <acrandal@wsu.edu>
2021-01-21 23:07:37 -0800

    Overhauled animations, added wave reset, added on/off toggle

--- PAJ7620_Controlled_RGB_LED/PAJ7620_Controlled_RGB_LED.ino
+++ PAJ7620_Controlled_RGB_LED/PAJ7620_Controlled_RGB_LED.ino
@@ -1,50 +1,63 @@
-#include "AlaLedRgb.h"
+/**
+ *  PAJ7620 Controlled RGB LEDs
+ * 
+ *  @author Aaron S. Crandall <crandall@gonzaga.edu>
+ *  @copyright 2020
+ *  
+ *  @license MIT
+ * 
+ */
 
+#include "AlaLedRgb.h"
 #include "RevEng_PAJ7620.h"
+
+bool g_LEDEnable = true;
 
 RevEng_PAJ7620 sensor = RevEng_PAJ7620();
 
 AlaLedRgb rgbStrip;
 
-int animation = 7;
+int animation = 0;
 int duration = 2000;
 int palette = 0;
 
-#define ANIMATION_CNT 14
+#define DEFAULT_ANIMATION  2
+#define DEFAULT_DURATION   5000
+#define DEFAULT_PALETTE    0
+
+#define ANIMATION_CNT 11
 #define PALETTE_CNT 3
 #define DURATION_MAX 10000
 
-int animList[14] = {
-    ALA_ON,
-    ALA_SPARKLE,
-    ALA_SPARKLE2,
-    ALA_PIXELSHIFTRIGHT,
+int animList[ANIMATION_CNT] = {
+    //ALA_ON,
+    ALA_LARSONSCANNER2,
+    ALA_FADEINOUT,
+    ALA_PLASMA,
+    //ALA_PIXELSHIFTRIGHT,
     ALA_PIXELSMOOTHSHIFTRIGHT,
     ALA_MOVINGBARS,
-    ALA_COMET,
+    //ALA_COMET,
     ALA_COMETCOL,
+    ALA_PIXELBOUNCE,
     ALA_GLOW,
     ALA_CYCLECOLORS,
-    ALA_FADECOLORS,
-    ALA_FIRE,
-    ALA_BOUNCINGBALLS,
+    //ALA_FADECOLORS,
+    //ALA_FIRE,
+    //ALA_BOUNCINGBALLS,
+    //ALA_SPARKLE,
+    ALA_SPARKLE2,
     ALA_BUBBLES
 };
 
-AlaPalette paletteList[3] = {
+AlaPalette paletteList[PALETTE_CNT] = {
     alaPalRgb,
     alaPalRainbow,
     alaPalFire
 };
 
-int durationList[4] = {
-    1000,
-    2000,
-    5000,
-    10000
-};
 
-
+// **** ***************************************************************************
 void setup()
 {
   Serial.begin(115200);
@@ -56,10 +69,13 @@ void setup()
   }
 
   rgbStrip.initWS2812(37, 8);
+  rgbStrip.setBrightness(AlaColor(100, 100, 100));
+  setALADefaults();
   updateAnimation();
 }
 
 
+// **** ***************************************************************************
 void loop()
 {
   Gesture gesture;                  // Gesture is an enum type from RevEng_PAJ7620.h
@@ -79,11 +95,46 @@ void loop()
     case GES_CLOCKWISE: decrementDuration(1000); break;
     case GES_ANTICLOCKWISE: incrementDuration(1000); break;
 
+    // Toggle lights on/off
+    case GES_FORWARD:
+      g_LEDEnable = !g_LEDEnable;
+      if( g_LEDEnable ) {
+        turnOn();
+      } else {
+        turnOff();
+      }
+      break;
+
+    case GES_WAVE:
+      if( sensor.getWaveCount() > 5 ) {
+        setALADefaults();
+        updateAnimation();
+      }
+      break;
 
   }
+  
   rgbStrip.runAnimation();
 }
 
+
+// **** ***************************************************************************
+void setALADefaults() {
+  animation = DEFAULT_ANIMATION;
+  duration = DEFAULT_DURATION;
+  palette = DEFAULT_PALETTE;
+}
+
+void turnOff() {
+  rgbStrip.setAnimation(ALA_OFF, 500, paletteList[palette%3]);  
+}
+
+void turnOn() {
+  updateAnimation();  
+}
+
+
+// **** ***************************************************************************
 void incrementPalette() {
   palette++;
   palette >= PALETTE_CNT ? palette = 0 : palette = palette;
@@ -122,10 +173,8 @@ void incrementDuration(int step_up) {
 }
 
 
-
+// **** ***************************************************************************
 void updateAnimation()
 {
-    //rgbStrip.setAnimation(animList[animation%14], durationList[duration%4], paletteList[palette%3]);
-  rgbStrip.setAnimation(animList[animation%14], duration, paletteList[palette%3]);
-
+  rgbStrip.setAnimation(animList[animation % ANIMATION_CNT], duration, paletteList[palette % PALETTE_CNT]);
 }
