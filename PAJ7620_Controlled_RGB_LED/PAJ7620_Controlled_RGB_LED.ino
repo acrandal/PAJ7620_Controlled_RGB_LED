a1e4f9ec1d6c98bc9e38afeb84e829f3180297a2
Aaron S. Crandall <acrandal@gmail.com>
2021-02-05 00:26:10 -0800

    Lots of updates, including idle off mode

--- PAJ7620_Controlled_RGB_LED/PAJ7620_Controlled_RGB_LED.ino
+++ PAJ7620_Controlled_RGB_LED/PAJ7620_Controlled_RGB_LED.ino
@@ -17,37 +17,53 @@ RevEng_PAJ7620 sensor = RevEng_PAJ7620();
 
 AlaLedRgb rgbStrip;
 
+
+#define TIMEOUT_MILLIS 10000
+long long int last_stamp = 0;
+
+enum Mode { ACTIVE, IDLING };
+Mode curr_mode = ACTIVE;
+
+
 int animation = 0;
 int duration = 2000;
 int palette = 0;
 
-#define DEFAULT_ANIMATION  2
+#define DEFAULT_ANIMATION  4
 #define DEFAULT_DURATION   5000
-#define DEFAULT_PALETTE    0
+#define DEFAULT_PALETTE    1
 
-#define ANIMATION_CNT 11
+#define ALA_BRIGHTNESS 25
+
+#define LED_COUNT 64
+#define LED_PIN A1
+
+#define ANIMATION_CNT 6
 #define PALETTE_CNT 3
 #define DURATION_MAX 10000
 
 int animList[ANIMATION_CNT] = {
     //ALA_ON,
     ALA_LARSONSCANNER2,
-    ALA_FADEINOUT,
-    ALA_PLASMA,
-    //ALA_PIXELSHIFTRIGHT,
     ALA_PIXELSMOOTHSHIFTRIGHT,
-    //ALA_COMET,
-    ALA_COMETCOL,
+    ALA_COMET,
     ALA_PIXELBOUNCE,
-    ALA_GLOW,
-    ALA_CYCLECOLORS,
+    ALA_SPARKLE2,
+    ALA_BUBBLES,
+
+    //ALA_PLASMA,           // --
+    //ALA_FIRE
+
+    //ALA_FADEINOUT,
+    //ALA_PIXELSHIFTRIGHT,
+    //ALA_COMETCOL,
+    //ALA_GLOW,
+    //ALA_CYCLECOLORS,
     //ALA_FADECOLORS,
-    //ALA_FIRE,
     //ALA_BOUNCINGBALLS,
     //ALA_SPARKLE,
-    ALA_SPARKLE2,
-    ALA_MOVINGBARS,
-    ALA_BUBBLES
+    //ALA_MOVINGBARS,
+
 };
 
 AlaPalette paletteList[PALETTE_CNT] = {
@@ -56,20 +72,42 @@ AlaPalette paletteList[PALETTE_CNT] = {
     alaPalFire
 };
 
+/*
+uint8_t gu_icon[8][8] = 
+{{   0, 255, 255,   0,   0,   0,   0,   0},
+ { 255,   0,   0, 255,   0,   0,   0,   0},
+ { 255,   0,   0,   0,   0,   0,   0,   0},
+ { 255,   0, 255, 255, 255, 255,   0, 255},
+ { 255,   0,   0, 255,   0, 255,   0, 255},
+ {   0, 255, 255,   0,   0, 255,   0, 255},
+ {   0,   0,   0,   0,   0, 255,   0, 255},
+ {   0,   0,   0,   0,   0,   0, 255,   0}};
+ */
 
 // **** ***************************************************************************
 void setup()
 {
   Serial.begin(115200);
 
+  last_stamp = millis();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  led_blink(1000);
+
+  Serial.println("Starting 7620");
   if( !sensor.begin() )           // return value of 0 == success
   {
-    Serial.print("PAJ7620 I2C error - halting");
+    Serial.println("PAJ7620 I2C error - halting");
     while(true) { }
   }
+  Serial.println("Done.");
 
-  rgbStrip.initWS2812(37, 8);
-  rgbStrip.setBrightness(AlaColor(100, 100, 100));
+  led_blink(1000);
+
+  Serial.print("Proceeding to lighting & processing");
+  rgbStrip.initWS2812(LED_COUNT, LED_PIN);
+  rgbStrip.setBrightness(AlaColor(ALA_BRIGHTNESS, ALA_BRIGHTNESS, ALA_BRIGHTNESS));
   setALADefaults();
   updateAnimation();
 }
@@ -84,7 +122,6 @@ void loop()
   switch (gesture)
   {
     // Control animation with left & right
-    //case GES_LEFT:    animation--; animation < 0 ? animation = ANIMATION_CNT - 1 : animation = animation; updateAnimation(); break;
     case GES_LEFT:    decrementAnimation(); break;
     case GES_RIGHT:   incrementAnimation(); break;
 
@@ -111,10 +148,24 @@ void loop()
         updateAnimation();
       }
       break;
-
   }
   
+  // Go into timeout idle mode
+  if( ( last_stamp + TIMEOUT_MILLIS < millis() ) && curr_mode == ACTIVE ) {  
+    curr_mode = IDLING;
+    turnOff();
+  }
+
   rgbStrip.runAnimation();
+}
+
+
+// **** ***************************************************************************
+void led_blink(int ms) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(ms);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(ms);
 }
 
 
@@ -176,5 +227,7 @@ void incrementDuration(int step_up) {
 // **** ***************************************************************************
 void updateAnimation()
 {
+  last_stamp = millis();
+  curr_mode = ACTIVE;
   rgbStrip.setAnimation(animList[animation % ANIMATION_CNT], duration, paletteList[palette % PALETTE_CNT]);
 }
